d928a5d12fc4498f448634584eae6decdcc370ef
enbraining <me@bricn.net>
2025-04-23 10:39:06 +0900

    :fire: 레디스 제거

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { RedisIoAdapter } from './socket/adapter/redis-adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,10 +8,6 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
-
-  const redisIoAdapter = new RedisIoAdapter(app);
-  await redisIoAdapter.connectToRedis();
-  app.useWebSocketAdapter(redisIoAdapter);
 
   await app.listen(3001);
 }
