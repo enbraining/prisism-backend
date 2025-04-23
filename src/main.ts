5eae0c1a511b53ffa50c9aac4d81f1b4199eeb7e
enbraining <me@bricn.net>
2025-04-23 10:50:50 +0900

    :recycle: CORS 설정

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: ['https://prisism-frontend.vercel.app', 'http://localhost:3000'],
     credentials: true,
   });
 
