25e27fe1d52d068b1315d2976e1799f7a2aed00c
enbraining <me@bricn.net>
2025-05-05 11:33:29 +0900

    add cors domain

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://prisism-frontend.vercel.app', 'http://localhost:3000'],
+    origin: [
+      'https://prisism-frontend.vercel.app',
+      'http://localhost:3000',
+      'https://prisism.com',
+      'https://www.prisism.com',
+    ],
     credentials: true,
   });
 
